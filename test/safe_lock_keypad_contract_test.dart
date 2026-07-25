ceb77520eec9c1372c67f01716cd7132db87f243
aljwaal1 <yaya15112016@gmail.com>
2026-07-25 10:36:22 +0300

    اختبار السماح بانطفاء شاشة الهاتف أثناء القفل

--- test/safe_lock_keypad_contract_test.dart
+++ test/safe_lock_keypad_contract_test.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('lock screen uses an internal keypad and does not wake the display', () {
+  test('lock screen uses an internal keypad and does not keep or wake the display', () {
     final source = File('native/MainActivityV2.kt').readAsStringSync();
     final lockStart = source.indexOf('class LockActivity : Activity()');
     final lockEnd = source.indexOf('class KidsMonnterDeviceAdminReceiver');
@@ -18,7 +18,9 @@ void main() {
     expect(lockSource, contains('unlockWithPin(addTime = false)'));
     expect(lockSource, contains('ScrollView(this)'));
     expect(lockSource, contains('.commit()'));
+    expect(lockSource, contains('FLAG_SHOW_WHEN_LOCKED'));
     expect(lockSource, isNot(contains('EditText(this)')));
+    expect(lockSource, isNot(contains('FLAG_KEEP_SCREEN_ON')));
     expect(lockSource, isNot(contains('FLAG_TURN_SCREEN_ON')));
     expect(lockSource, isNot(contains('FLAG_DISMISS_KEYGUARD')));
   });
