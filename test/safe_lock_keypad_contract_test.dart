a8642e385cfcda0f9d9e379eceb6ca696ba900ff
aljwaal1 <yaya15112016@gmail.com>
2026-07-25 07:34:36 +0300

    إضافة اختبار عقد لوحة القفل الآمنة

---
+++ test/safe_lock_keypad_contract_test.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('lock screen uses an internal keypad and does not wake the display', () {
+    final source = File('native/MainActivityV2.kt').readAsStringSync();
+    final lockStart = source.indexOf('class LockActivity : Activity()');
+    final lockEnd = source.indexOf('class KidsMonnterDeviceAdminReceiver');
+    expect(lockStart, greaterThanOrEqualTo(0));
+    expect(lockEnd, greaterThan(lockStart));
+    final lockSource = source.substring(lockStart, lockEnd);
+
+    expect(lockSource, contains('GridLayout(this)'));
+    expect(lockSource, contains('enteredPin'));
+    expect(lockSource, contains('refreshPinUi()'));
+    expect(lockSource, contains('unlockWithPin(addTime = true)'));
+    expect(lockSource, contains('unlockWithPin(addTime = false)'));
+    expect(lockSource, contains('ScrollView(this)'));
+    expect(lockSource, contains('.commit()'));
+    expect(lockSource, isNot(contains('EditText(this)')));
+    expect(lockSource, isNot(contains('FLAG_TURN_SCREEN_ON')));
+    expect(lockSource, isNot(contains('FLAG_DISMISS_KEYGUARD')));
+  });
+}
