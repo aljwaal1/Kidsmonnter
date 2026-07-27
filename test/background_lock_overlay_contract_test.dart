0634c5e3978462389ffc861526f77c62d5ec42ff
aljwaal1 <yaya15112016@gmail.com>
2026-07-27 22:05:21 +0300

    تحديث اختبار القفل لمسار Android 8.1 الموثوق

--- test/background_lock_overlay_contract_test.dart
+++ test/background_lock_overlay_contract_test.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('يحافظ القفل التلقائي على النافذة ويضيف طبقة kiosk عند توفر Device Owner', () {
+  test('يحافظ القفل التلقائي على النافذة ويفتح النشاط الكامل عند دعمه', () {
     final native = File('native/MainActivityV2.kt').readAsStringSync();
     final serviceStart = native.indexOf('class MonitorService');
     final serviceEnd = native.indexOf('class LockActivity');
@@ -19,12 +19,12 @@ void main() {
     expect(service, contains('stopProtectionFromBackgroundLock()'));
     expect(service, contains('dismissLockOverlay()'));
     expect(service, contains('.removeViewImmediate(view)'));
-    expect(service, contains('launchLockActivityIfDeviceOwner()'));
+    expect(service, contains('launchLockActivityReliably()'));
     expect(service, contains('Intent(this, LockActivity::class.java)'));
     expect(service, contains('FLAG_SECURE'));
     expect(service, contains('FLAG_ALT_FOCUSABLE_IM'));
     expect(service, contains('systemGestureExclusionRects'));
-    expect(service, contains('LOCK_ACTIVITY_REASSERTED'));
+    expect(service, contains('LOCK_ACTIVITY_STARTED'));
     expect(service, contains('releaseDeviceOwnerPolicies()'));
     expect(service, contains('stopSelf()'));
     expect(
